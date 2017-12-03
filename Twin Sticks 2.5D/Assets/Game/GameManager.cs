74a5bf4234ed0fc3d02424575d5be1ee206f9010
HeroChan <andrew@swiftevolutions.com>
2017-12-02 22:59:07 -0500

    Added Everything finished game, added pause, added some objects, made camera follow ball, and added limits

---
+++ Twin Sticks 2.5D/Assets/Game/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool recording;
+    public bool paused = false;
+    public GameObject Pause;
+
+    private float fixedDeltaTime;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start() {
+        fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            recording = false;
+        }
+        else
+        {
+            recording = true;
+        }
+
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        Time.fixedDeltaTime = 0;
+        Pause.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = fixedDeltaTime;
+        Pause.SetActive(false);
+
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Twin Sticks 2.5D/Assets/Game/ReplaySystem1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem1 : MonoBehaviour {
+
+    private const int BufferFrames = 100;
+    private myKeyFrame[] KeyFrames = new myKeyFrame[BufferFrames];
+
+    private Rigidbody rigidBody;
+
+    private GameManager manager;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        manager = GameManager.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Record();
+        if(manager.recording)
+        {
+            Record();
+        }
+        else
+        {
+            PlayBack();
+        }
+	}
+
+    public void PlayBack()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % BufferFrames;
+        print("Reading frame " + frame);
+        transform.position = KeyFrames[frame].pos;
+        transform.rotation = KeyFrames[frame].rotation;
+    }
+
+    public void Record()
+    {
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % BufferFrames;
+        float time = Time.time;
+        print("Writing Frame " + frame);
+
+        KeyFrames[frame] = new myKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+public struct myKeyFrame
+{
+    public float frameTime;
+    public Vector3 pos;
+    public Quaternion rotation;
+
+    public myKeyFrame(float atime, Vector3 aPosition, Quaternion aRotation)
+    {
+        frameTime = atime;
+        pos = aPosition;
+        rotation = aRotation;
+    }
+}
+
